e83a6ad935a8529f79967d17f8f0daebd685cb05
axel8890 <axel8890@gmail.com>
2018-12-02 16:47:31 +0100

    cosis

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject FinishButton, CityPanel, RecoursesPanel, MergePanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PlayerTurn()
+    {
+        FinishButton.SetActive(true);
+
+    }
+    void EnemyTurn()
+    {
+        FinishButton.SetActive(false);
+
+    }
+}
