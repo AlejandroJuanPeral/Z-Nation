1940c55df4f64eec88fdd145596372754bcac941
Verónica Martuscello Baixauli <al341511@uji.es>
2018-12-11 11:45:47 +0100

    Models Okis

---
+++ Assets/Vero/Scripts/Unidad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Unidad : MonoBehaviour
+{
+    public Enumerados.Position posicion { get; }
+
+    public Unidad(Enumerados.Position posicion)
+    {
+        this.posicion = posicion;
+    }
+}
